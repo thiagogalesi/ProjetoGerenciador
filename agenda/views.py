11600b4a559e7f18a3cddc86c8a97924fd80348a
Thiago Galesi <thiagogalesi@gmail.com>
2012-10-26 21:16:34 +0100

    no class based views

--- agenda/views.py
+++ agenda/views.py
@@ -1,8 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.views.generic import View
 
+def main(request):
+    return HttpResponse('Ola mundo!')
 
-class Agenda(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello, World!')
