30d6cba8b073879aad074ad4ec00f1c5acacb03e
Thiago Galesi <thiagogalesi@gmail.com>
2012-11-17 08:52:16 +0000

    agenda admin

---
+++ agenda/admin.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+
+from agenda.models import ItemAgenda
+from django.contrib import admin
+
+class ItemAgendaAdmin(admin.ModelAdmin):
+    fields = ('data', 'hora', 'titulo', 'descricao')
+    list_display = ('data', 'hora', 'titulo')
+
+    def queryset(self, request):
+        qs = super(ItemAgendaAdmin, self).queryset(request)
+        return qs.filter(usuario=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.usuario = request.user
+        obj.save()
+
+admin.site.register(ItemAgenda, ItemAgendaAdmin)
+
