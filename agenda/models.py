384488141f1f4fcfb2c385ae90c3fc8afd8134d2
Thiago Galesi <thiagogalesi@gmail.com>
2012-11-18 21:23:22 +0000

    mock send email

--- agenda/models.py
+++ agenda/models.py
@@ -10,3 +10,9 @@ class ItemAgenda(models.Model):
     usuario = models.ForeignKey(User)
     participantes = models.ManyToManyField(User, related_name='item_participantes')
 
+def envia_email(**kwargs):
+    print ("Enviando email")
+
+models.signals.post_save.connect(envia_email,
+        sender=ItemAgenda,
+        dispatch_uid="agenda.models.ItemAgenda")
