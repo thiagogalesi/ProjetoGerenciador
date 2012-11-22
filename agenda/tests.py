5b4537dadb52732cabaf9ebd77e6c257ae3c28b7
Thiago Galesi <thiagogalesi@gmail.com>
2012-11-22 21:27:08 +0000

    basic tests

--- agenda/tests.py
+++ agenda/tests.py
@@ -6,9 +6,22 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from agenda.models import ItemAgenda
+from django.contrib.auth.models import User
+import datetime
 
 class SimpleTest(TestCase):
+    def setUp(self):
+        usuario = User.objects.create_user('test_user', password='test_password')
+        agora = datetime.datetime.now()
+        item = ItemAgenda.objects.create(data=agora.date(), hora=agora.time(), titulo="Teste", 
+            descricao="Descricao teste", usuario=usuario)
+        item.save()
+
+    def test_busca(self):
+        item = ItemAgenda.objects.get(titulo="Teste")
+        self.assertEquals(item.descricao, "Descricao teste")
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
